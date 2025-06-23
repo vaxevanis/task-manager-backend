ade37e4d3f16b90702a80e860d6aef22323be1ae
paulv <vaxevanis.paul@gmail.com>
2025-06-23 18:38:49 +0200

    feat: create shared auth package

---
+++ packages/auth/src/constants.ts
@@ -0,0 +1,2 @@
+export const JWT_SECRET = process.env.JWT_SECRET || "change-me";
+export const ROLES_KEY = "roles";
---
+++ packages/auth/src/index.ts
@@ -0,0 +1,3 @@
+export * from "./roles.decorator";
+export * from "./roles.guard";
+export * from "./constants";
