4cc725c4e3d32f8c7ccd15ea7fc0718f3ff71006
paulv <vaxevanis.paul@gmail.com>
2025-06-23 19:14:52 +0200

    feat: create authModule in api-gateway

---
+++ apps/api-gateway/src/auth/jwt.strategy.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { JWT_SECRET } from '@task-manager/auth';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: JWT_SECRET,
+    });
+  }
+
+  async validate(payload: any) {
+    return payload; // Customize as needed
+  }
+}
