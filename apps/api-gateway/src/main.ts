21d3348bc29ed1b2c15ffc87d610a794d9ee4516
paulv <vaxevanis.paul@gmail.com>
2025-06-25 18:33:36 +0200

    feat(api-gateway): validation pipe

--- apps/api-gateway/src/main.ts
+++ apps/api-gateway/src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, // strips unknown properties
+      forbidNonWhitelisted: true, // throws if extra properties
+      transform: true, // converts payload to DTO instance
+    }),
+  );
   await app.listen(process.env.PORT ?? 3000);
 }
 // This will raise a lint warning a (@typescript-eslint/no-floating-promises)
